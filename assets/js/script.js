d3853b0d45a5d3798b54cb493acd65c0b96c6999
Kieran Collins <kiecol@protonmail.com>
2022-11-18 02:11:11 +0000

    Add submit button, add const questions

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+const QUESTIONS = [
+{
+  "question": "What manufacturer made the Countach",
+  "option-a": "Fiat",
+  "option-b": "Ferrari",
+  "option-c": "Lamborghini",
+  "option-d": "Alfa Romeo",
+  "correct": "Lamborghini"
+},
+{
+  "question": "What manufacturer made the i8",
+  "option-a": "Audi",
+  "option-b": "Jaguar",
+  "option-c": "Lexus",
+  "option-d": "BMW",
+  "correct": "BMW"
+}
+]
