2dd5028c0b41dae76a1370b31cd620751cc08186
Kieran Collins <kiecol@protonmail.com>
2022-11-18 16:55:19 +0000

    Resolve issue loading question. Review and modify all id, classess, etc.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,7 @@
 const startButton = document.getElementById('start-button')
 const quizQuestion = document.getElementById('question-div')
-const quizOptions = document.getElementById('quiz-options')
 const questionElement = document.getElementById('question')
-const optionButtons = document.getElementsByClassName('option-button')
+const optionButtons = document.getElementById('quiz-options')
 
 // The below variables are assigned in the startQuiz function. They are used to randomize the order of the questions. 
 let shuffleQuestions, currentQuestionIndex
@@ -10,20 +9,19 @@ let shuffleQuestions, currentQuestionIndex
 startButton.addEventListener('click', startQuiz)
 
 /**
- * Activated when user clicks start.
+ * Activates when user clicks start quiz button.
  */
 function startQuiz() {
   console.log('The quiz has started, please select an answer, good luck!') // Logs message to console
   startButton.classList.add('hide') // Hides the Start Button
   quizQuestion.classList.remove('hide') // Shows Quiz Question
-  quizOptions.classList.remove('hide') // Shows Quiz Options
   shuffleQuestions = questions.sort(() => Math.random() -.5) // Randomizes the order of the questions array
-  currenntQuestionIndex = 0 // We are starting the list of questions from 0
+  currentQuestionIndex = 0 // We are starting the list of questions from 0
   nextQuestion() // Initiates the nextQuestion Function
 }
 
 function nextQuestion() {
-  displayQuestion(shuffleQuestions[currentQuestionIndex])
+  displayQuestion(shuffleQuestions[currentQuestionIndex]) 
 }
 
 function displayQuestion(question) {
@@ -36,12 +34,22 @@ function selectOption() {
 
 const questions = [
   {
-    "question": "What manufacturer made the i8",
-    "option-a": "Audi",
-    "option-b": "Jaguar",
-    "option-c": "Lexus",
-    "option-d": "BMW",
-    "correct": "BMW"
+    question: 'Which manufacturer made the Countach',
+    options: [
+      { text:'Fiat', correct: false },
+      { text:'Ferrari', correct: false },
+      { text:'Alfa Romeo', correct: false },
+      { text:'Lamborghini', correct: true }
+    ]
+  },
+  {
+    question: 'Which manufacturer made the i8',
+    options: [
+      { text:'Audi', correct: false },
+      { text:'BMW', correct: true },
+      { text:'Jaguar', correct: false },
+      { text:'Lexus', correct: false }
+    ]
   }
   ]
 
@@ -72,7 +80,7 @@ const questions = [
       { text:'Fiat', correct: false },
       { text:'Ferrari', correct: false },
       { text:'Alfa Romeo', correct: false },
-      { text:'Lamborghini', correct: true },
+      { text:'Lamborghini', correct: true }
     ]
   },
   {
@@ -81,7 +89,7 @@ const questions = [
       { text:'Audi', correct: false },
       { text:'BMW', correct: true },
       { text:'Jaguar', correct: false },
-      { text:'Lexus', correct: false },
+      { text:'Lexus', correct: false }
     ]
   }
    */
