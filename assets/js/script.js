bf1405294051f1aacbc109e49608aa51acc8a670
Kieran Collins <kiecol@protonmail.com>
2022-11-18 14:15:21 +0000

    Add code to add / remove elements when user starts the quiz in the startQuiz function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
 const startButton = document.getElementById('start-button')
+const quizQuestion = document.getElementById('question-div')
+const quizOptions = document.getElementById('quiz-options')
 
 startButton.addEventListener('click', startQuiz)
 
@@ -6,8 +8,10 @@ startButton.addEventListener('click', startQuiz)
  * Activated when user clicks start.
  */
 function startQuiz() {
-  console.log('The quiz has started')
+  console.log('The quiz has started, please select an answer, good luck!')
   startButton.classList.add('hide')
+  quizQuestion.classList.remove('hide')
+  quizOptions.classList.remove('hide')
 }
 
 function nextQuestion() {
@@ -36,3 +40,5 @@ const QUESTIONS = [
     "correct": "BMW"
   }
   ]
+
+  console.log('The page has loaded and the quiz is ready to start!')
