c751e17192b023223890751cd6d43d7326f8f56e
Kieran Collins <kiecol@protonmail.com>
2022-11-18 13:56:41 +0000

    Add start button. Add JS start game function. Layout JS functions.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,38 @@
-const QUESTIONS = [
-{
-  "question": "What manufacturer made the Countach",
-  "option-a": "Fiat",
-  "option-b": "Ferrari",
-  "option-c": "Lamborghini",
-  "option-d": "Alfa Romeo",
-  "correct": "Lamborghini"
-},
-{
-  "question": "What manufacturer made the i8",
-  "option-a": "Audi",
-  "option-b": "Jaguar",
-  "option-c": "Lexus",
-  "option-d": "BMW",
-  "correct": "BMW"
+const startButton = document.getElementById('start-button')
+
+startButton.addEventListener('click', startQuiz)
+
+/**
+ * Activated when user clicks start.
+ */
+function startQuiz() {
+  console.log('The quiz has started')
+  startButton.classList.add('hide')
 }
-]
+
+function nextQuestion() {
+
+}
+
+function selectOption() {
+
+}
+
+const QUESTIONS = [
+  {
+    "question": "What manufacturer made the Countach",
+    "option-a": "Fiat",
+    "option-b": "Ferrari",
+    "option-c": "Lamborghini",
+    "option-d": "Alfa Romeo",
+    "correct": "Lamborghini"
+  },
+  {
+    "question": "What manufacturer made the i8",
+    "option-a": "Audi",
+    "option-b": "Jaguar",
+    "option-c": "Lexus",
+    "option-d": "BMW",
+    "correct": "BMW"
+  }
+  ]
