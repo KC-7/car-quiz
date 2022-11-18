4499d73921ca7301da224d7bc3540b0963760676
Kieran Collins <kiecol@protonmail.com>
2022-11-18 15:47:23 +0000

    Add code to randomise question order and move to next question

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
 const startButton = document.getElementById('start-button')
 const quizQuestion = document.getElementById('question-div')
 const quizOptions = document.getElementById('quiz-options')
+const questionElement = document.getElementById('question')
+const optionButtons = document.getElementsByClassName('option-button')
+
+// The below variables are assigned in the startQuiz function. They are used to randomize the order of the questions. 
+let shuffleQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startQuiz)
 
@@ -8,29 +13,28 @@ startButton.addEventListener('click', startQuiz)
  * Activated when user clicks start.
  */
 function startQuiz() {
-  console.log('The quiz has started, please select an answer, good luck!')
-  startButton.classList.add('hide')
-  quizQuestion.classList.remove('hide')
-  quizOptions.classList.remove('hide')
+  console.log('The quiz has started, please select an answer, good luck!') // Logs message to console
+  startButton.classList.add('hide') // Hides the Start Button
+  quizQuestion.classList.remove('hide') // Shows Quiz Question
+  quizOptions.classList.remove('hide') // Shows Quiz Options
+  shuffleQuestions = questions.sort(() => Math.random() -.5) // Randomizes the order of the questions array
+  currenntQuestionIndex = 0 // We are starting the list of questions from 0
+  nextQuestion() // Initiates the nextQuestion Function
 }
 
 function nextQuestion() {
+  displayQuestion(shuffleQuestions[currentQuestionIndex])
+}
 
+function displayQuestion(question) {
+  questionElement.innerHTML = question.question
 }
 
 function selectOption() {
 
 }
 
-const QUESTIONS = [
-  {
-    "question": "What manufacturer made the Countach",
-    "option-a": "Fiat",
-    "option-b": "Ferrari",
-    "option-c": "Lamborghini",
-    "option-d": "Alfa Romeo",
-    "correct": "Lamborghini"
-  },
+const questions = [
   {
     "question": "What manufacturer made the i8",
     "option-a": "Audi",
@@ -42,3 +46,42 @@ const QUESTIONS = [
   ]
 
   console.log('The page has loaded and the quiz is ready to start!')
+
+  /**
+   * {
+    "question": "",
+    "option-a": "Fiat",
+    "option-b": "",
+    "option-c": "",
+    "option-d": "",
+  },
+  {
+    "question": "What manufacturer made the i8",
+    "option-a": "Audi",
+    "option-b": "Jaguar",
+    "option-c": "Lexus",
+    "option-d": "BMW",
+    "correct": "BMW"
+  }
+
+
+
+    {
+    question: 'Which manufacturer made the Countach',
+    options: [
+      { text:'Fiat', correct: false },
+      { text:'Ferrari', correct: false },
+      { text:'Alfa Romeo', correct: false },
+      { text:'Lamborghini', correct: true },
+    ]
+  },
+  {
+    question: 'Which manufacturer made the i8',
+    options: [
+      { text:'Audi', correct: false },
+      { text:'BMW', correct: true },
+      { text:'Jaguar', correct: false },
+      { text:'Lexus', correct: false },
+    ]
+  }
+   */
